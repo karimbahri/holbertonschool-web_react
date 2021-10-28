493d219f9ae42be0b9efeb1d6b1f6ed92fd89abe
karim <karim-bahri-18@outlook.fr>
2021-10-28 20:34:26 +0100

    task3

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import $ from 'jquery';
+
+let i = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+    i++;
+    $('#count').text(i + ' clicks on the button');
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(`${__dirname}`, 'public'),
+        filename: 'bundle.js'
+    }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+            test: /\.(jpg|png|git|svg)$/,
+            loader: 'image-webpack-loader'
+            }
+        ]
+    }
+}

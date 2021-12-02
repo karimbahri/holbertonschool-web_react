15b61a41645d9f715e40528e89adf01018565d06
karim bahri <karim-bahri-18@outlook.fr>
2021-12-02 15:36:28 +0100

    nested

---
+++ 0x07-React_Immutable/2-nested.js
@@ -0,0 +1,5 @@
+import getImmutableObject from "./0-fromjs";
+import { getIn } from './node_modules/immutable/dist/immutable';
+
+const accessImmutableObject = (object, array) =>  getImmutableObject(object).getIn(array);
+export default accessImmutableObject;

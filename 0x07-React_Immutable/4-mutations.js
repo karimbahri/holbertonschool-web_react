0a3348bdb0d94b863f79b6fbc4679157911ef6c9
karim bahri <karim-bahri-18@outlook.fr>
2021-12-02 15:57:50 +0100

    4-mutation

---
+++ 0x07-React_Immutable/4-mutations.js
@@ -0,0 +1,13 @@
+import { Map } from './node_modules/immutable/dist/immutable';
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+const map2 = map.withMutations( element => element.set(2, 'Benjamin').set(4, 'Oliver') );
+
+export {map, map2};
